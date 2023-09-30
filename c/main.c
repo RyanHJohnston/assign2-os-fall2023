f5b39d639d0c82324c1f9193dd1ca0190a20196a
ryanhjohnston <ryanhjohnstoncollege@gmail.com>
2023-09-30 01:36:32 -0500

    Got the threads part 2 working, JESUS CHRIST

--- c/main.c
+++ c/main.c
@@ -1,4 +1,5 @@
 
+
 #include <bits/time.h>
 #include <errno.h>
 #include <stddef.h>
@@ -7,7 +8,7 @@
 #include <stdint.h>
 #include <pthread.h>
 #include <string.h>
-#include <ctype.h>
+#include <ctype.h> 
 #include <time.h>
 
 #define DOUBLE_MIN 0.0
@@ -20,6 +21,13 @@ typedef struct ThreadData {
     double *avg;
 } ThreadData;
 
+typedef struct MergedThreadData {
+    struct ThreadData *th_data_0;
+    struct ThreadData *th_data_1;
+    struct ThreadData *th_merged_data;
+} MergedThreadData;
+
+
 /****************** HELPER FUNCTIONS ******************************/
 /* Basic Help Functions */
 void print_array(double *array, int length);
@@ -29,7 +37,7 @@ void copy_array(double *src, double *dest, int length, int index);
 
 /* Thread Functions */
 void *sort_thread_avg(void *arg);
-void *merge_thread_avg(void *arg0, void *arg1);
+void *merge_thread_avg(void *arg);
 
 
 /****************** DRIVER **********************************/
@@ -58,6 +66,7 @@ main(int argc, char *argv[])
     pthread_t tid_1;
     pthread_t tid_2;
     pthread_t tid_3;
+    pthread_t tid_4;
 
     // Dynamic arrays
     double elapsed;
@@ -102,15 +111,15 @@ main(int argc, char *argv[])
     }
     
     // Assign n_half = n / 2
-    n_half = n / 2;
+    n_half = (n + 1) / 2;
 
     // Allocate the arrays
     A_byte_size = sizeof(double) * n;
     A = (double*)malloc(A_byte_size);
     B = (double*)malloc(A_byte_size);
     C = (double*)malloc(A_byte_size);
-    A_first_half = (double*)malloc( A_byte_size / 2);
-    A_second_half = (double*)malloc( A_byte_size / 2);
+    A_first_half = (double*)malloc(n_half * sizeof(double));
+    A_second_half = (double*)malloc(n_half * sizeof(double));
 
     srand((unsigned int)time(NULL)); // init rand seed, ensure rand values on different runs
 
@@ -136,7 +145,7 @@ main(int argc, char *argv[])
     
     /* Sorting is done in 10.0ms when one thread is used */
     fprintf(stdout, "Sorting is done in %f when one thread is used\n",
-            elapsed);
+            elapsed * 1000);
     if (n < 10) {
         fprintf(stdout, "Average of sorted array: %10f ms\n", *B_data->avg);
         print_array(B_data->array, n);
@@ -151,46 +160,129 @@ main(int argc, char *argv[])
 
     /*********** TWO THREADS CASE *********************/
     /* copy A into A_first_half and A_second_half */
-    copy_array(A, A_first_half, n_half, COPY_ARRAY_INDEX);
-    copy_array(A, A_second_half, n_half, COPY_ARRAY_INDEX);
+    /* size_t num_elements = 100; */
+    /* int* originalArray = malloc(num_elements * sizeof(int)); */
+    /* size_t half_elements = (num_elements + 1) / 2;  // this will round up if odd */
+
+    /* // Populate the original array as an example */
+    /* for (size_t i = 0; i < num_elements; i++) { */
+    /*     originalArray[i] = i; */
+    /* } */
+
+    /* // Allocate memory for the second half */
+    /* int* secondHalfArray = malloc(half_elements * sizeof(int)); */
+
+    /* // Copy the second half of originalArray to secondHalfArray */
+    /* memcpy(secondHalfArray, &originalArray[num_elements / 2], half_elements * sizeof(int)); */
+    
+    /* memcpy(A_first_half, &A[n / 2], n_half * sizeof(double)); */
+    /* memcpy(A_second_half, &A[n - (n / 2)], n_half * sizeof(double)); */
+    
+        printf("\nSTARTING TWO THREADS CASE\n\n");
+    printf("\nCopying First half array\n");
+    for (i = 0; i < n_half; ++i) {
+        printf("A[i]: %f\n", A[i]);
+        A_first_half[i] = A[i];
+    }
+    int j = 0;
+    printf("\nCopying Second half array\n\n");
+    for (i = n_half; i < n; ++i) {
+        printf("A[i]: %f\n", A[i]);
+        A_second_half[j] = A[i];
+        j++;
+    }
+ 
+
+
+
     clock_gettime(CLOCK_MONOTONIC, &ts_begin);
     // create threadA1 sortThread_avg to sort AFirsthalf and compute Afirstavg
     // Do the same for the second thread
     ThreadData *A_first_half_data = (ThreadData*)malloc(sizeof(ThreadData));
     ThreadData *A_second_half_data = (ThreadData*)malloc(sizeof(ThreadData));
+    
+    
     A_first_half_data->array = A_first_half;
+    printf("A first half data array was assigned\n");
+    print_array(A_first_half_data->array, n_half);
     A_first_half_data->length = n_half;
+
+
     A_second_half_data->array = A_second_half;
+    printf("A second half datay array was assigned\n");
+    print_array(A_second_half_data->array, n_half);
     A_second_half_data->length = n_half;
     
+    printf("\nSTARTING 2 PTHREAD_CREATE\n\n");
     pthread_create(&tid_2, NULL, sort_thread_avg, (void *)A_first_half_data);
     pthread_create(&tid_3, NULL, sort_thread_avg, (void *)A_second_half_data);
+    printf("\nSTARTING 2 PTHREAD JOIN\n\n");
     pthread_join(tid_2, (void **)&A_first_half_data);
     pthread_join(tid_3, (void **)&A_second_half_data);
-    
+
     // create a thM megaThread_avg by passing A_first_half, A_second_half, A_first_avg, A_second_avg
-    // make sure this just merges the sorted values from two arrays while keeping them sorted O(n)
-    // don't copy these array sand then call sort! which will be like sorting the whole thing!
-    // it's supposed to simply compute overall_avg = (A_first_avg + a_first_avg) / 2
-    // return merged/sorted array and overall_avg
+    MergedThreadData *merged_data = (MergedThreadData*)malloc(sizeof(MergedThreadData));
+    merged_data->th_data_0 = malloc(sizeof(ThreadData));
+    merged_data->th_data_1 = malloc(sizeof(ThreadData));
+    merged_data->th_merged_data = malloc(sizeof(ThreadData));
+
+    merged_data->th_data_0->array = A_first_half_data->array;
+    merged_data->th_data_1->array = A_second_half_data->array;
+    merged_data->th_data_0->avg = A_first_half_data->avg;
+    merged_data->th_data_1->avg = A_second_half_data->avg;
+    merged_data->th_data_0->length = A_first_half_data->length;
+    merged_data->th_data_1->length = A_second_half_data->length; 
+
+
+    /* ThreadData *merged_data = (ThreadData*)malloc(sizeof(ThreadData)); */
+    /* MergedThreadData *merged_data = (MergedThreadData*)malloc(sizeof(MergedThreadData)); */
+    /* merged_data->th_data_0->array = A_first_half_data->array; */
+    /* merged_data->th_data_1->array = A_second_half_data->array; */
+    /* merged_data->th_data_0->avg = A_first_half_data->avg; */
+    /* merged_data->th_data_1->avg = A_second_half_data->avg; */
+    /* merged_data->th_data_0->length = A_first_half_data->length; */
+    /* merged_data->th_data_1->length = A_second_half_data->length; */
+    /* merged_data->th_merged_data = (ThreadData*)malloc(sizeof(ThreadData)); */
     
+    printf("\nSTARTING MERGE THREAD AVGERAGE CREATE\n\n");
+    pthread_create(&tid_4, NULL, merge_thread_avg, (void *)merged_data);
+    printf("\nSTARTING MERGE THREAD AVERAGE JOIN\n\n");
+    pthread_join(tid_4, (void **)&merged_data);
+    printf("\nFNISHED MERGED DATA PTHREAD JOIN\n\n");
+
+    clock_gettime(CLOCK_MONOTONIC, &ts_end);
+    elapsed = ts_end.tv_sec - ts_begin.tv_sec;
+    elapsed += (ts_end.tv_nsec - ts_begin.tv_nsec) /  1000000000.0;
     
-    // join thM
+    printf("\nSTARTING TO PRINT OUT RESULTS\n\n");
+
     
 
-    /* Sorting by TWO threads is done in elasped * 1000 ms */
-    /* Print overall_avg and at least first 10 values of the sorted array */
     
 
 
 
 
     /*********** FREE MEMORY *****************/
+    free(A_first_half_data->avg);
+    free(A_second_half_data->avg);
+    free(A_first_half_data);
+    free(A_second_half_data);
+    free(merged_data->th_data_0);
+    free(merged_data->th_data_1);
+    free(merged_data->th_merged_data);
+    free(merged_data);
     free(A);
     free(B);
     free(C);
     free(A_first_half);
     free(A_second_half);
+
+    /* free(A); */
+    /* free(B); */
+    /* free(C); */
+    /* free(A_first_half); */
+    /* free(A_second_half); */
 
     return 0;
 }
@@ -362,109 +454,71 @@ sort_thread_avg(void *arg)
     pthread_exit(data);
 }
 
-
-
-void *
-merge_thread_avg(void *arg0, void *arg1)
+void*
+merge_thread_avg(void *arg)
 {
-    ThreadData *data_0;
-    ThreadData *data_1;
-    ThreadData *merged_data;
+    MergedThreadData *merged_thread_data;
     
-    double *data_0_array;
-    double *data_1_array;
-    double *merged_data_array;
-
-    double *data_0_avg;
-    double *data_1_avg;
-    double *merged_data_avg;
-
-    double data_0_sum = 0.0;
-    double data_1_sum = 0.0;
-    double merged_data_sum = 0.0;
-
-    int data_0_length = 0;
-    int data_1_length = 0;
-    int merged_data_lengh = 0;
-
-    int i = 0;
-
-    /* assign the args */
-    data_0 = (ThreadData*)arg0;
-    data_1 = (ThreadData*)arg1;
-    data_0_array = (double*)malloc(sizeof(double) * data_0->length);
-    data_1_array = (double*)malloc(sizeof(double) * data_1->length);
+    double *array;
+    double *avg;
+    double sum = 0.0;
+    int length;
+    int i;
     
-    /* check for successful memory allocation */
-    if (!data_0_array || !data_1_array) {
-        fprintf(stderr, "Memory was not successfully allocated to arrays ih merge_thread_avg\n");
-        free(data_0_array);
-        free(data_1_array);
+    
+
+    merged_thread_data = (MergedThreadData*)arg;
+    length = (merged_thread_data->th_data_0->length + 
+            merged_thread_data->th_data_1->length);
+    array = (double*)malloc(sizeof(double) * length);
+    
+    if (!array) {
+        fprintf(stderr, "Unsuccessfull malloc to array in merge_thread_avg\n");
+        free(array);
         pthread_exit(NULL);
     }
+  
+    merged_thread_data->th_merged_data->array = 
+        (double*)malloc(sizeof(double) * length);
 
-    /* assign length */
-    data_0_length = data_0->length;
-    data_1_length = data_1->length;
-    
-    /* copy arrays */
-    copy_array(data_0->array, data_0_array, data_0_length, COPY_ARRAY_INDEX);
-    copy_array(data_1->array, data_1_array, data_1_length, COPY_ARRAY_INDEX);
-    
-    /* allocate memory for averages */
-    data_0_avg = (double*)malloc(sizeof(double));
-    data_1_avg = (double*)malloc(sizeof(double));
+    memcpy(
+            array,
+            merged_thread_data->th_data_0->array,
+            merged_thread_data->th_data_0->length * sizeof(double));
+    memcpy(
+            array + merged_thread_data->th_data_0->length,
+            merged_thread_data->th_data_1->array,
+            merged_thread_data->th_data_1->length * sizeof(double));
 
-    /* check for successful memory allocation of averages */
-    if (!data_0_avg || !data_1_avg) {
-        fprintf(stdout, "Memory was not successfully allocated to averages in merge_thread_avg\n");
-        free(data_0_avg);
-        free(data_1_avg);
-        pthread_exit(NULL);
+    for (i = 0; i < length; ++i) {
+        printf("merged_array[i]: %f\n", array[i]);
     }
 
-    *data_0_avg = data_0->avg;
-    *data_1_avg = data_1->avg;
+    avg = (double*)malloc(sizeof(double));
+    if (!avg) {
+        fprintf(stderr, "Unsuccessfull malloc to avg in merge_thread_avg\n");
+        pthread_exit(NULL);
+    }
+    
+    selection_sort(array, length);
+    
+    printf("\nMEMCPY\n\n");
+    memcpy(
+            merged_thread_data->th_merged_data->array,
+            array,
+            length * sizeof(double));
+    printf("\nEND OF MEMCPY\n\n");
+    *avg = (*merged_thread_data->th_data_0->avg + *merged_thread_data->th_data_1->avg) / 2;
+    merged_thread_data->th_merged_data->avg = avg;
+    merged_thread_data->th_merged_data->length = length;
 
-    return NULL; // keep this here for now    
+    free(array);
+    
+    pthread_exit(merged_thread_data);
 }
 
 
 
-    /* function mergeSortedArrays(A, B): */
-    /* n = length(A) */
-    /* m = length(B) */
-    /* C = new array of size (n + m) */
-    
-    /* i = 0   // pointer for array A */
-    /* j = 0   // pointer for array B */
-    /* k = 0   // pointer for merged array C */
-
-    /* while i < n and j < m: */
-    /*     if A[i] <= B[j]: */
-    /*         C[k] = A[i] */
-    /*         i = i + 1 */
-    /*     else: */
-    /*         C[k] = B[j] */
-    /*         j = j + 1 */
-    /*     k = k + 1 */
-
-    /* // If there are remaining elements in A */
-    /* while i < n: */
-    /*     C[k] = A[i] */
-    /*     i = i + 1 */
-    /*     k = k + 1 */
-
-    /* // If there are remaining elements in B */
-    /* while j < m: */
-    /*     C[k] = B[j] */
-    /*     j = j + 1 */
-    /*     k = k + 1 */
-
-    /* return C */
-
-    
-    
 
 
 
